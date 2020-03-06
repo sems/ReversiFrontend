4eff04f8b304ac01c533935295bb70b2d3c2f504
Sem <me@sems.dev>
2020-03-06 14:59:01 +0100

    Gulp watch
    Gulp watch

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,5 +1,7 @@
 const config = require('./config');
+const {series, parallel, watch} = require('gulp');
 
+const browserSync = require('browser-sync').create();
 const js = require('./tasks/js').js(config.files.js, config.files.jsOrder, config.localServerProjectPath);  
 const sass  = require('./tasks/js').sass(config.files.sass, config.localServerProjectPath)
 
@@ -8,7 +10,20 @@ const hello = function (done) {
     done();
 }
 
+const watchFiles = () => {
+    browserSync.init({server: {baseDir: './'}});
+ 
+    watch('./css/*.sass', series(sass));
+    watch('./css/*.scss', series(sass));
+ 
+    watch('./css/*.scss').on('change', browserSync.reload);
+    watch('./css/*.sass').on('change', browserSync.reload);
+ }; 
+ 
+watchFiles.displayName = 'watch';
+
 //Exports
 exports.default = hello;
 exports.js = js;
 exports.sass = sass;
+exports.watch = watchFiles;
