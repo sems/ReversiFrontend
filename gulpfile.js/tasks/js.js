c9a0ae58a7a5a655641a8847da8d4808f1b3303b
Sem <me@sems.dev>
2020-03-10 21:32:06 +0100

    Grid
    Made CSS grid.

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -3,7 +3,7 @@ const order = require('gulp-order');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const gulpSass = require('gulp-sass');
-
+const uglify = require('gulp-uglify');
 
 const fn = function (filesJs, filesJsOrder, backendPath) {  
     return function () {  
@@ -17,6 +17,7 @@ const fn = function (filesJs, filesJsOrder, backendPath) {
             presets: ['@babel/preset-env']
         }))
         .pipe(dest('./dist/js'))
+        .pipe(uglify({compress: true}))  
         .pipe(dest(backendPath + "js/")); 
     }  
 };  
--- js/game.reversi.js
+++ js/game.reversi.js
@@ -11,9 +11,22 @@ Game.Reversi = (function () {
         console.log("private init Module");
     }
 
+    const showFiche = function (kleur, x, y) {
+        if(kleur == 0) {
+            $('.' + 'div_' + x + '_' + y).removeClass("fiche-black");
+            $('.' + 'div_' + x + '_' + y).removeClass("fiche-white");
+            //$('.' + 'div_' + x + '_' + y).addClass("fiche-none");
+        } else {
+            var color = kleur == 1 ? 'white' : 'black';
+            //$('.' + 'div_' + x + '_' + y).removeClass("fiche-none");
+            $('.' + 'div_' + x + '_' + y).addClass("fiche-" + color);
+        }
+    }
+
     // Waarde/object geretourneerd aan de outer scope
     return {
-        init: privateInit
+        init: privateInit,
+        showFiche: showFiche
     }
 
 })()
