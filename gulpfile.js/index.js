4650594621c2d0b59b3713df265128eaa9a7f0d2
Sem <me@sems.dev>
2020-03-17 09:38:40 +0100

    HTML and Sass
    Added html and sass gulp stuff

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -4,6 +4,7 @@ const {series, parallel, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const js = require('./tasks/js').js(config.files.js, config.files.jsOrder, config.localServerProjectPath);  
 const sass  = require('./tasks/js').sass(config.files.sass, config.localServerProjectPath)
+const htmlTask = require('./tasks/js').htmlTask('./', config.localServerProjectPath);
 
 const hello = function (done) {
     console.log(`Groeten van ${config.voornaam}!`)
@@ -29,4 +30,5 @@ watchFiles.displayName = 'watch';
 exports.default = hello;
 exports.js = js;
 exports.sass = sass;
+exports.htmlTask = htmlTask;
 exports.watch = watchFiles;
--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -4,6 +4,10 @@ const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const gulpSass = require('gulp-sass');
 const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 
 const fn = function (filesJs, filesJsOrder, backendPath) {  
     return function () {  
@@ -28,11 +32,33 @@ const sass = function (files_sass, backendPath) {
         .pipe(gulpSass().on('error', gulpSass.logError))
         .pipe(dest('./dist/css'))
         .pipe(gulpSass({outputStyle: 'compressed'}))
-        .pipe(concat('main.min.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename('main.min.css'))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
         .pipe(dest('./dist/css'))
         .pipe(dest(backendPath + "css/"));
 	}
 };
 
+const htmlTask = function (path, backendPath) {
+    return function () {
+        return src(['index.html'])
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            minifyJS: true,
+            minifyCSS: true,
+            removeComments: true
+        }))
+        .pipe(rename(function (path) {
+            path.dirname += "/";
+            path.basename = 'index';
+            path.extname = ".html";
+        }))
+        .pipe(dest('./dist/'))
+        .pipe(dest(backendPath));
+    }
+}
+
 exports.sass = sass; 
 exports.js = fn;  
+exports.htmlTask = htmlTask;
