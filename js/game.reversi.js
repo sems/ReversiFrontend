2ec512bcff61b308a4b35d9e5bad331be772fc26
Sem Spanhaak <me@sems.dev>
2020-03-27 12:50:07 +0100

    Adding fiches to board

--- js/game.reversi.js
+++ js/game.reversi.js
@@ -2,21 +2,27 @@ Game.Reversi = (function () {
     console.log('hallo, vanuit module Reversi')
 
     //Configuratie en state waarden
-    let configMap = {
-
+    let _configMap = {
+        apiPath: '/api/spel/',
+        idOfGame: 0
     }
 
     // Private function init
-    const privateInit = function () {
-        console.log("Game.Template starting...");
+    const _init = function (id) {
+        console.log("Game.Template starting...")
+        _configMap.idOfGame = id
+        getGameState(id)
     }
 
-    const showFiche = function (kleur, x, y) {
+    const showFiche = function (kleur, y, x) {
         if(kleur == 0) {
             $('.' + 'div_' + x + '_' + y).removeClass("fiche-black");
             $('.' + 'div_' + x + '_' + y).removeClass("fiche-white");
             //$('.' + 'div_' + x + '_' + y).addClass("fiche-none");
         } else {
+            $('.' + 'div_' + x + '_' + y).removeClass("fiche-black");
+            $('.' + 'div_' + x + '_' + y).removeClass("fiche-white");
+
             var color = kleur == 1 ? 'white' : 'black';
             //$('.' + 'div_' + x + '_' + y).removeClass("fiche-none");
             $('.' + 'div_' + x + '_' + y).addClass("fiche-" + color);
@@ -38,11 +44,38 @@ Game.Reversi = (function () {
         }))
     }
 
+    const getGameState = function(id) {
+        $.get(_configMap.apiPath + id)
+        .then((data) => {
+            let bord = data.bord
+            for (let i = 0; i < 7; i++) {
+                for (let j = 0; j < 7; j++) {
+                    const element = bord[i][j];
+                    showFiche(element, i, j)
+                }   
+            }
+        })
+    }
+
+    const Move = function (y, x) {
+        $.get(_configMap.apiPath + _configMap.idOfGame + "/" + y + "/" + x)
+        .then((response) => {
+            console.log(response);
+            getGameState(_configMap.idOfGame)
+        })
+    }
+
+    const updateChartData = function() {
+        
+    }
+
     // Waarde/object geretourneerd aan de outer scope
     return {
-        init: privateInit,
+        init: _init,
         showFiche: showFiche,
         clearBoard: clearBoard,
-        showQuote: showQuote
+        showQuote: showQuote,
+        getGameState: getGameState,
+        Move: Move
     }
 })()
